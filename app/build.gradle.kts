518972a32f5deec8683b31a65afc95f2327fd80a
MAN2ll <a.n.moiseev89@gmail.com>
2026-05-29 15:32:05 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.securevault"
+        applicationId = "com.securevault1"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
