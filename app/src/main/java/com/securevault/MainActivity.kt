ae0c545fda559aa56b124f562067fefe5bece39a
MAN2ll <a.n.moiseev89@gmail.com>
2026-05-30 10:30:03 +0300

    Update MainActivity.kt

--- app/src/main/java/com/securevault/MainActivity.kt
+++ app/src/main/java/com/securevault/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.securevault
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.*
 import com.securevault.ui.SecureVaultNavHost
 import com.securevault.ui.theme.SecureVaultTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent { SecureVaultTheme { SecureVaultNavHost() } }
+        setContent {
+            // Определяем тему: светлая/тёмная/системная
+            val systemDark = isSystemInDarkTheme()
+            SecureVaultTheme(darkTheme = systemDark) {
+                SecureVaultNavHost()
+            }
+        }
     }
 }
